dd9a1acce21daf28888e3052634674d7ea6986ed
Administrator <Administrator@PC-20180420QNNV>
2018-05-30 11:35:49 +0800

    提交读取资源

---
+++ src/main/java/mytest/vertclusertest/jason/loader/ResourceLoader.java
@@ -0,0 +1,65 @@
+package mytest.vertclusertest.jason.loader;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * 
+ * @author 有泪的北极星 qq: 76598166
+ * @date 2018年5月30日 上午11:09:09
+ */
+public class ResourceLoader {
+
+	private static ResourceLoader instance = new ResourceLoader();
+
+	private static Map<String, Properties> configMap = new HashMap<>();
+
+	/** 默认的配置文件 */
+	private static String DEFAULT_CONFIG_FILE = "vertx.properties";
+
+	public static ResourceLoader getInstance() {
+		return instance;
+	}
+
+	/**
+	 * 获取配置文件
+	 * 
+	 * @param fileName
+	 * @return
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public Properties getProperties(String fileName) throws FileNotFoundException, IOException {
+		Properties properties = configMap.get(fileName);
+		if (properties != null) {
+			return properties;
+		}
+		String path = this.getClass().getClassLoader().getResource(fileName).getPath();
+		properties = new Properties();
+		properties.load(new FileInputStream(new File(path)));
+		configMap.put(fileName, properties);
+		return properties;
+	}
+
+	private String getValue(String key) throws FileNotFoundException, IOException {
+		return getProperties(DEFAULT_CONFIG_FILE).getProperty(key);
+	}
+
+	public Object getValue(String key, Object type) throws FileNotFoundException, IOException {
+		if (type instanceof Boolean) {
+			return Boolean.parseBoolean(getValue(key));
+		} else if (type instanceof Integer) {
+			return Integer.parseInt(getValue(key));
+		} else if (type instanceof Long) {
+			return Long.parseLong(getValue(key));
+		} else if (type instanceof Double) {
+			return Double.parseDouble(getValue(key));
+		}
+		return null;
+	}
+}
